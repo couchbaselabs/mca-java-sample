a0b5869ab5c69c18995005ba76c90bc624174db5
mgroves <me@mgroves.com>
2017-11-29 14:57:52 -0500

    added a basic loop to do writes and show number of writes every second

--- src/main/java/couchbase/webinar/HelloWorld.java
+++ src/main/java/couchbase/webinar/HelloWorld.java
@@ -1,5 +1,5 @@
 package couchbase.webinar;
-/*
+
 import com.couchbase.client.java.Bucket;
 import com.couchbase.client.java.Cluster;
 import com.couchbase.client.java.CouchbaseCluster;
@@ -10,13 +10,64 @@ import com.couchbase.client.java.query.N1qlQuery;
 import com.couchbase.client.java.query.N1qlQueryResult;
 import com.couchbase.client.java.query.N1qlQueryRow;
 import com.couchbase.client.java.query.consistency.ScanConsistency;
-*/
+
 import java.io.Console;
-// import java.util.UUID;
+import java.time.Duration;
+import java.util.UUID;
+import java.time.Instant;
+import java.util.Random;
 
 public class HelloWorld {
-    public static void main(String... args) throws Exception {
-        System.out.println("Hello, webinar.");
+
+        private static Bucket bucket;
+        private static Integer writes;
+        private static Random rand;
+
+        public static void main(String... args) throws Exception {
+
+                // initialize couchbase
+                Cluster cluster = CouchbaseCluster.create("localhost");
+                cluster.authenticate("webinaruser", "password");
+                bucket = cluster.openBucket("webinar");
+
+                // initialize counter(s) and such
+                rand = new Random();
+                writes = 0;
+
+                // loop forever, doing writes
+                // show the number of writes every second since the last second
+                Instant start = Instant.now();
+                while(true)
+                {
+                        Instant now = Instant.now();
+
+                        Write();
+
+                        if(Duration.between(start, now).getSeconds() >= 1) {
+                                System.out.println("Writes: " + writes);
+                                start = Instant.now();
+                                writes = 0;
+                        }
+                }
+        }
+
+        public static void Write() {
+                // Create a JSON Document
+                JsonObject person = JsonObject.create()
+                        .put("firstName", "John")
+                        .put("lastName", "Doe")
+                        .put("type", "person");
+
+                // create a key "person::#" where # is 1 to 1000
+                String key = "p::" + rand.nextInt(1000) + 1; //UUID.randomUUID().toString();
+
+                // Store the Document
+                bucket.upsert(JsonDocument.create(key, person));
+
+                // increment the number of writes
+                writes++;
+        }
+}
         /*
         // Initialize the Connection
         Cluster cluster = CouchbaseCluster.create("localhost");
@@ -58,5 +109,4 @@ public class HelloWorld {
         
         cluster.disconnect();
 */
-    }
-}
+
