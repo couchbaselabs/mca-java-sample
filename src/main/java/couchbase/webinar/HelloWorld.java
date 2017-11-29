65d659602f372a5b9e7a041fda4fdf907b454691
mgroves <me@mgroves.com>
2017-11-29 15:15:44 -0500

    added faker for fake names added Read() setup 1:2 ratio for writes to reads

--- src/main/java/couchbase/webinar/HelloWorld.java
+++ src/main/java/couchbase/webinar/HelloWorld.java
@@ -16,12 +16,15 @@ import java.time.Duration;
 import java.util.UUID;
 import java.time.Instant;
 import java.util.Random;
+import com.github.javafaker.Faker;
 
 public class HelloWorld {
 
         private static Bucket bucket;
         private static Integer writes;
+        private static Integer reads;
         private static Random rand;
+        private static Faker faker;
 
         public static void main(String... args) throws Exception {
 
@@ -32,21 +35,30 @@ public class HelloWorld {
 
                 // initialize counter(s) and such
                 rand = new Random();
+                faker = new Faker();
                 writes = 0;
+                reads = 0;
 
-                // loop forever, doing writes
-                // show the number of writes every second since the last second
+                // loop forever, doing reads and writes
                 Instant start = Instant.now();
                 while(true)
                 {
                         Instant now = Instant.now();
 
-                        Write();
+                        // write 33% of the time, read 67% of the time
+                        Integer split = rand.nextInt(3);
+                        if(split == 1)
+                                Write();
+                        else
+                                Read();
 
+                        // if 1 second has elapsed, show the counts and reset them for the
+                        // next second
                         if(Duration.between(start, now).getSeconds() >= 1) {
-                                System.out.println("Writes: " + writes);
+                                System.out.println("Writes: " + writes + ", Reads: " + reads);
                                 start = Instant.now();
                                 writes = 0;
+                                reads = 0;
                         }
                 }
         }
@@ -54,12 +66,12 @@ public class HelloWorld {
         public static void Write() {
                 // Create a JSON Document
                 JsonObject person = JsonObject.create()
-                        .put("firstName", "John")
-                        .put("lastName", "Doe")
+                        .put("firstName", faker.name().firstName())
+                        .put("lastName", faker.name().lastName())
                         .put("type", "person");
 
                 // create a key "person::#" where # is 1 to 1000
-                String key = "p::" + rand.nextInt(1000) + 1; //UUID.randomUUID().toString();
+                String key = "p::" + rand.nextInt(1000) + 1;
 
                 // Store the Document
                 bucket.upsert(JsonDocument.create(key, person));
@@ -67,46 +79,15 @@ public class HelloWorld {
                 // increment the number of writes
                 writes++;
         }
-}
-        /*
-        // Initialize the Connection
-        Cluster cluster = CouchbaseCluster.create("localhost");
-        Bucket bucket = cluster.openBucket("default");
 
-        //Generate a unique id for key creation
-        String uniqueID = UUID.randomUUID().toString();
-        String key = "p:" + uniqueID;
+        public static void Read() {
+                // create a key "person::#" where # is 1 to 1000
+                String key = "p::" + rand.nextInt(1000) + 1;
 
-        // Create a JSON Document
-        JsonObject person = JsonObject.create()
-                .put("firstName", "John")
-                .put("lastName", "Doe")
-                .put("city", "San Francisco")
-                .put("country", "United States")
-                .put("type", "person");
+                // Store the Document
+                bucket.get(key);
 
-
-        // Store the Document
-        bucket.upsert(JsonDocument.create(key, person));
-
-        // Create a N1QL Primary Index (but ignore if it exists)
-        bucket.bucketManager().createN1qlPrimaryIndex(true, false);
-
-        // Perform N1QL Query. Use request plus consistency to read your write immediately.
-        N1qlQuery query =  N1qlQuery.parameterized("SELECT meta(p).id AS documentKey, p.firstName, p.lastName " +
-                        "FROM default AS p WHERE type = $type",
-                JsonObject.create().put("type", "person"),
-                N1qlParams.build().consistency(ScanConsistency.REQUEST_PLUS));
-
-        N1qlQueryResult result = bucket.query(query);
-
-        for (N1qlQueryRow row : result) {
-            System.out.println("Key: " + row.value().getString("documentKey") + "\t" +
-                    "FirstName: " + row.value().getString("firstName") + "\t" +
-                    "LastName: " + row.value().getString("lastName") + "\t"
-            );
+                // increment the number of writes
+                reads++;
         }
-        
-        cluster.disconnect();
-*/
-
+}
